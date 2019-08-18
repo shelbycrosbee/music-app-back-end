f6acdaa6a190e2df181e55a8fccebef36fb5b269
Shelby Miller <shelbycrosbee@gmail.com>
2019-08-18 17:28:21 -0600

    remigrated to adjust for active users and joined playlists

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -7,8 +7,16 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
+      table.boolean('active')
+      table.string('display_name', 254)
       table.string('spotify_id', 80).notNullable().unique()
+
+      //only for storing user's personal playlist
       table.string('uri_link', 254)
+
+      //the stream that this person is listening to && their display_name
+      table.string('topic_id', 254)
+      table.string('playlist_master', 254)
       table.timestamps()
     })
   }
